bd69a929c9c043b7f2aaec15eab5fb6047b34057
Hacı Celal Aygar <celal.aygar@gmail.com>
2026-06-24 00:29:08 +0300

    Delete src directory

--- src/components/DynamicNavbar.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const Navbar = dynamic(() => import('@/components/sections/Navbar'), { ssr: false })
-
-export default function DynamicNavbar() {
-  return <Navbar />
-}
